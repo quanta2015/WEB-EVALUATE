b82eedd6f24e5c725cdf1525d81f729b66d41917
yogi-ye <37772406+yogi-ye@users.noreply.github.com>
2018-07-06 00:12:23 +0800

    Create student.js

---
+++ src/sys/js/student.js
@@ -0,0 +1,21 @@
+(function(jq, g) {
+
+    var arr = [
+    {
+        'begin': '2018-7-4',
+        'end': '2018-7-10',
+        'state': 1   
+    },
+    {
+        'begin': '2018-7-11',
+        'end': '2018-7-21',
+        'state': 0   
+    }
+    ],
+        //获取模板
+        jsRenderTpl = $.templates('#testTmpl'),
+        //末班与数据结合
+        finalTpl = jsRenderTpl(arr);
+
+        $("#list").html(finalTpl);
+})(jQuery, window);
