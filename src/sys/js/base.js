03cb7db84e406e81c62e581fc641a4ccbb9edcf5
YOEmma <38882725+YOEmma@users.noreply.github.com>
2018-07-05 17:10:47 +0800

    Update base.js

--- src/sys/js/base.js
+++ src/sys/js/base.js
@@ -1,48 +1,65 @@
-// RegExp
-function performRequest(url, method, data, successCallback, errorCallback, completeCallback) {
-  var method = method || "POST"
-  var url = url || "/"
-  var data = data || {}
-  var success = successCallback || function(result) {
-    console.log("call '" + url + "' successfully")
-  }
-  var error = errorCallback || function(err) {
-    console.log("call '" + url + "' failed, because " + err.responseText)
-  }
-  var complete = completeCallback || function(e){
-    console.log("call '" + url + "' completed")
-  }
+        $().ready(function () {
 
-  if (method == "POST") {
-    data = JSON.stringify(data)
-  }
+            $("#form").validate({
+                rules: {
+                    name: "required",
+                    pwd: "required",
 
-  $.ajax({
-    method: method,
-    url: url,
-    data: data,
-    dataType: "json",
-    contentType: "application/json",
-    success: success,
-    error: error,
-    complete: complete
-  })
-}
+                },
+                messages: {
+                    name: "请输入您的账号",
+                    pwd: {
+                        required: "请输入密码"
+                    }
+                }
+            });
+        });
 
-function getURLParameter(sParam) {
-  var sPageURL = window.location.search.substring(1)
-  var sURLVariables = sPageURL.split('&')
-  for (var i = 0; i < sURLVariables.length; i++) {
-    var sParameterName = sURLVariables[i].split('=')
-    if (sParameterName[0] == sParam) {
-      return sParameterName[1]
-    }
-  }
-
-  return undefined
-}
-
-function showError(err) {
-  $('#notice .alert .message').text('[' + err.code + '] ' + errMap[err.code] || err.message)
-  $('#notice .alert').removeClass('alert-success').addClass('alert-danger').show()
-}
+        //// RegExp
+        //function performRequest(url, method, data, successCallback, errorCallback, completeCallback) {
+        //  var method = method || "POST"
+        //  var url = url || "/"
+        //  var data = data || {}
+        //  var success = successCallback || function(result) {
+        //    console.log("call '" + url + "' successfully")
+        //  }
+        //  var error = errorCallback || function(err) {
+        //    console.log("call '" + url + "' failed, because " + err.responseText)
+        //  }
+        //  var complete = completeCallback || function(e){
+        //    console.log("call '" + url + "' completed")
+        //  }
+        //
+        //  if (method == "POST") {
+        //    data = JSON.stringify(data)
+        //  }
+        //
+        //  $.ajax({
+        //    method: method,
+        //    url: url,
+        //    data: data,
+        //    dataType: "json",
+        //    contentType: "application/json",
+        //    success: success,
+        //    error: error,
+        //    complete: complete
+        //  })
+        //}
+        //
+        //function getURLParameter(sParam) {
+        //  var sPageURL = window.location.search.substring(1)
+        //  var sURLVariables = sPageURL.split('&')
+        //  for (var i = 0; i < sURLVariables.length; i++) {
+        //    var sParameterName = sURLVariables[i].split('=')
+        //    if (sParameterName[0] == sParam) {
+        //      return sParameterName[1]
+        //    }
+        //  }
+        //
+        //  return undefined
+        //}
+        //
+        //function showError(err) {
+        //  $('#notice .alert .message').text('[' + err.code + '] ' + errMap[err.code] || err.message)
+        //  $('#notice .alert').removeClass('alert-success').addClass('alert-danger').show()
+        //}
