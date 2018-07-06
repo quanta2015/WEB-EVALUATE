20d4ee121d57c4a8b9b27894e16ad224902b195a
陶娣 <454101322@qq.com>
2018-07-06 22:15:41 +0800

    update pages

---
+++ src/sys/js/evalute.js
@@ -0,0 +1,25 @@
+(function(jq, g) {
+
+	var arr = [
+	{
+		'taskID': '001',
+		'stdID':'2016210405054',
+		'class':'软工162',
+		'name':'陶娣',
+		'isEvalute':'0'
+	},
+	{
+		'taskID': '001',
+		'stdID':'2016210405050',
+		'class':'软工162',
+		'name':'叶艳洁',
+		'isEvalute':'1'
+	}
+	],
+	//获取模版
+	jsRenderTpl = $.templates('#theTmpl'),
+	//模版与数据结合
+	finalTpl = jsRenderTpl(arr);
+
+	$('.box').html(finalTpl);
+})(jQuery, window);
