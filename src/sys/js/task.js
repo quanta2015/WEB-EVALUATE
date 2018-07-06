3961e4df76210d55ec458c99884e315b065c7460
YOEmma <38882725+YOEmma@users.noreply.github.com>
2018-07-06 10:49:55 +0800

    Update task.js

--- src/sys/js/task.js
+++ src/sys/js/task.js
@@ -1,17 +1,8 @@
-        $(init)
-
-        function init() {
-            $(".btn").on('click', doSubmit)
-            $('.dropdown-toggle').dropdown()
-            $('#startDatetimepicker').datetimepicker();
-            $('#endDatetimepicker').datetimepicker();
-
-            let clsArr = ['软工162', '物联网161', '物联网162']
-            clsArr.forEach((v) => {
-                $('#u-cls li').append(`<a role="menuitem" tabindex="-1" href="#">${v}</a>`)
-            })
-        }
-
-        function doSubmit() {
-
-        }
+        $(document).ready(function() {
+            var clsname = ['软工162', '软工161', '物联网161', '物联网162'];
+            var clsval;
+            for (i = 0, len = clsname.length; i < len; i++) {
+                clsval = clsname[i];
+                $("#clsCh").append("<option>" + clsval + "</option>");
+            }
+        });
