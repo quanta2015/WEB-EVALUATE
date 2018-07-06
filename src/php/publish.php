0c3e27f78c81557415ca89315003e25aefad32d8
slcyyy <37917457+slcyyy@users.noreply.github.com>
2018-07-06 17:17:24 +0800

    Update publish.php

--- src/php/publish.php
+++ src/php/publish.php
@@ -1,7 +1,7 @@
 <?php
-
+  require_once 'conn.php';
 header("Content-type: text/html; charset=utf-8");
-header("Access-Control-Allow-Origin: *");
+
 
 session_start();
 if (isset($_POST['submit'])) {
@@ -12,19 +12,21 @@ if (isset($_POST['submit'])) {
     $wd = $_POST["task_wd"];
     $pt = $_POST["task_pt"];
     $vd = $_POST["task_vd"];
+    $publisher = $_SESSION['name'];
+
     if ($class == "" || $title == "" || $begin == "" || $end == "" || $wd == "" || $pt == "" || $vd == "") {
         echo "<script>alert('信息不能为空，请重新填写');history.go(-1);</script>";
     }
     else {
-        require_once 'conn.php';
+      
         if (!$conn) {
             die('Could not connect: ' . mysqli_connect_error());
         }
-        mysqli_query($conn, "SET NAMES UTF8");
-        $query = "insert into task (publish_class,task_title,task_content_wd,task_content_pt,task_content_vd,publish_date,end_date) values('{$_POST['class']}','{$_POST['title']}','{$_POST['task_wd']}','{$_POST['task_pt']}','{$_POST['task_vd']}','{$_POST['begin']}','{$_POST['end']}')";
-        
-        $result=mysqli_query($conn, $query);
-        echo "<script>alert('发布成功!');location.href='../pages/task.html';</script>";
+      
+        $insert_sql = "insert into task(publish_class)values('{$class}'，'{$publisher}',{$title}','{$begin}','{$end}',{$wd}','{$pt}','{$vd}')";
+     
+        $result = $conn->query($insert_sql);
+        echo "<script>alert('发布成功!');location.href='../pages/homework.html';</script>";
     }
     mysqli_free_result($result);
 }
