3d53d2657880c7a8511ae9ca4a104b985cc47a8b
Euodia <38934133+Euodia@users.noreply.github.com>
2018-07-07 18:13:59 +0800

    将发布改成API接口

--- src/php/publish.php
+++ src/php/publish.php
@@ -1,28 +1,67 @@
 <?php
 require_once 'conn.php';
+require_once 'common.php';
 header("Content-type: text/html; charset=utf-8");
 session_start();
-if (isset($_POST['submit'])) {
-    $class = $_POST["class"];
-    $title = $_POST["title"];
-    $begin = $_POST["begin"];
-    $end = $_POST["end"];
-    $wd = $_POST["task_wd"];
-    $pt = $_POST["task_pt"];
-    $vd = $_POST["task_vd"];
-    $publisher = $_SESSION['name'];
-    if ($class == "" || $title == "" || $begin == "" || $end == "" || $wd == "" || $pt == "" || $vd == "") {
-        echo "<script>alert('信息不能为空，请重新填写');history.go(-1);</script>";
-    } 
-    else {
-        if (!$conn) {
-            die('Could not connect: ' . mysqli_connect_error());
-        }
-        $_SESSION['title'] = $title;
-        $insert_sql = "insert into task(publish_class,task_title,publish_date,end_date,task_content_wd,task_content_pt,task_content_vd,publisher)values('{$class}','{$title}','{$begin}','{$end}','{$wd}','{$pt}','{$vd}','{$publisher}')";
-        //print_r($insert_sql);
-        $result = mysqli_query($conn, $insert_sql);
-        echo "<script>alert('发布成功!');location.href='../pages/evalute.html';</script>";
+$_SESSION['name'] = 'admin';
+
+$class = $_POST["class"];
+$title = $_POST["title"];
+$time = $_POST["daterange"];
+$wd = $_POST["task_wd"];
+$pt = $_POST["task_pt"];
+$vd = $_POST["task_vd"];
+$publisher = $_SESSION['name'];
+   //echo $class. $title. $begin. $end. $wd. $pt. $vd. $publisher;
+if ($class == "" || $title == "" || $time== "" || $wd == "" || $pt == "" || $vd == "") {
+    $response = array(
+         'code' => 80,
+         "msg"=>"existempty",
+
+         );
+        $response = CodeUtil::jsons_encode($response);
+        header("Content-Type:text/html;charset=utf-8");
+        echo urldecode(json_encode($response));
+} 
+else {
+    $time= explode(" - ", $time);
+    $begin=date_format(date_create($time[0]), 'Y-m-d H:i:s');
+    $end=date_format(date_create($time[1]), 'Y-m-d H:i:s');
+
+
+
+    if (!$conn) {
+         $response = array(
+         'code' => 55,
+         "msg"=>"connectfail",
+         );
+        $response = CodeUtil::jsons_encode($response);
+        header("Content-Type:text/html;charset=utf-8");
+        die (urldecode(json_encode($response)));
     }
+    $insert_sql = "insert into task(publish_class,task_title,publish_date,end_date,task_content_wd,task_content_pt,task_content_vd,publisher)values('{$class}','{$title}','{$begin}','{$end}','{$wd}','{$pt}','{$vd}','{$publisher}')";
+    
+   // print_r($insert_sql);
+   if(1==mysqli_query($conn,$insert_sql)){
+    $response = array(
+         'code' => 0,
+         "msg"=>"success",
+         );
+        $response = CodeUtil::jsons_encode($response);
+        header("Content-Type:text/html;charset=utf-8");
+        die (urldecode(json_encode($response)));}
+
+     $response = array(
+         'code' => 20,
+         "msg"=>"unknownfail",
+         );
+        $response = CodeUtil::jsons_encode($response);
+        header("Content-Type:text/html;charset=utf-8");
+        echo (urldecode(json_encode($response)));
+
+
+    
 }
+
 mysqli_close($conn);
+
