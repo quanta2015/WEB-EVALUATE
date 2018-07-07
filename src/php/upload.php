7fba05865c6a378533ef018fc8241d32f3f01dfa
Xuan0216 <849651526@qq.com>
2018-07-07 19:41:10 +0800

    upload file

--- src/php/upload.php
+++ src/php/upload.php
@@ -1,47 +1,38 @@
 <?php
-// 允许上传的文件后缀
-$allowedExts = array("doc", "docx", "ppt", "pptx","avi","flv","mp4","wmv");
-$temp = explode(".", $_FILES["file"]["name"]);
-echo $_FILES["file"]["size"];
-$extension = end($temp);     // 获取文件后缀名
-if ((($_FILES["file"]["type"] == "application/msword")
-|| ($_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-|| ($_FILES["file"]["type"] == "application/vnd.ms-powerpoint")
-|| ($_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.presentationml.presentation")
-|| ($_FILES["file"]["type"] == "video/avi")
-|| ($_FILES["file"]["type"] == "application/octet-stream")
-|| ($_FILES["file"]["type"] == "video/mp4")
-|| ($_FILES["file"]["type"] == "video/x-ms-wmv"))
-&& ($_FILES["file"]["size"] < 6000000)   // 小于 6M
-&& in_array($extension, $allowedExts))
-{
-	if ($_FILES["file"]["error"] > 0)
-	{
-		echo "错误：: " . $_FILES["file"]["error"] . "<br>";
+
+header("Content-type: text/html; charset=utf-8");
+
+if($_POST['submit']=='提交作业'){
+	print_r($_FILES);
+	function delEmpty($v){
+		return $v!='';
 	}
-	else
-	{
-		echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-		echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
-		echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-		echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
-		
-		// 判断当期目录下的 upload 目录是否存在该文件
-		// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-		if (file_exists("upload/" . $_FILES["file"]["name"]))
-		{
-			echo $_FILES["file"]["name"] . " 文件已经存在。 ";
-		}
-		else
-		{
-			// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-			move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-			echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
+	$newUpFileName = array_filter($_FILES['file']['name'],'delEmpty');
+	print_r($newUpFileName);
+	$dirName = 'upload/';
+	if(!is_dir($dirName)){
+		mkdir($dirName,0777,1);
+	}
+	foreach($newUpFileName as $k=>$v){
+		if(is_uploaded_file($_FILES['file']['tmp_name'][$k])){
+			$upFileDir = $dirName.'/'.$_FILES['file']['name'][$k];
+			if(move_uploaded_file($_FILES['file']['tmp_name'][$k],$upFileDir)){
+				echo '<script type="text/javascript">
+					alert("上传成功！");
+					history.go(-1);
+				</script>';
+			}else{
+				echo '<script type="text/javascript">
+					alert("移动失败！");
+					history.go(-1);
+				</script>';
+			}
+		}else{
+			echo '<script type="text/javascript">
+				alert("不是通过http post上传的文件，请重新操作");
+				history.go(-1);
+			</script>';
 		}
 	}
-}
-else
-{
-	echo "非法的文件格式";
 }
 ?>
