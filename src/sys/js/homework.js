ff32b525de63d995ee3f953ad7466b1f17309530
yogi-ye <37772406+yogi-ye@users.noreply.github.com>
2018-07-09 00:02:19 +0800

    Update homework.js

--- src/sys/js/homework.js
+++ src/sys/js/homework.js
@@ -1,9 +1,6 @@
+var arr = new Array();//状态数组
+
 $(document).on("click", "#submit", function (e) {
-  //   for(i=1; i<4; i++){
-  //   if(document.getElementById('v'+i).value == ''){
-  //     document.getElementById('v'+i).name = 'uu';
-  //   }
-  // }
     bootbox.confirm({
         message: "确认提交作业？",
         buttons: {
@@ -18,11 +15,27 @@ $(document).on("click", "#submit", function (e) {
         },
         callback: function(result) {
             if(result) {
-                // alert('点击了确认按钮');
-                // location.href="student.html?state=1";
-                url = "student.html?state=1";//此处拼接内容
+                i = $("#head").text();
+                arr[i-1] = "1";
+                url = "student.html?state=1"+"&i="+i+"&a=1"+"&arr="+arr;//此处拼接内容
                 window.location.href = url;
             } 
         }
     });
 });
+
+
+
+$(function(){
+    getData();
+});
+
+function getData(){
+    var i = $.query.get("i");
+    var s = $.query.get("s");
+
+    for(var j=0;j<((s.length+1)/2);++j){
+        arr[j] = s[(j+1)*2-2];
+    }
+    $("#head").text(i);
+}
