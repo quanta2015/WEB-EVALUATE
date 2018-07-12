1ecd316f5a09777a2a53e6dbdbc04f7a5c84a083
yogi-ye <37772406+yogi-ye@users.noreply.github.com>
2018-07-12 22:03:33 +0800

    Update homework.js

--- src/sys/js/homework.js
+++ src/sys/js/homework.js
@@ -1,33 +1,31 @@
 var arr = new Array();//状态数组
 
-//点击提交按钮
-$(document).on("click", "#submit1", function (e) {
-    console.log(1);
-    bootbox.confirm({
-        message: "确认提交作业？",
-        buttons: {
-            confirm: {
-                label: 'Yes',
-                className: 'btn-success'
+$(document).ready(function(){
+    //点击提交按钮
+    $("#submit").click(function(e){
+        bootbox.confirm({
+            message: "确认提交作业？",
+            buttons: {
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'No',
+                    className: 'btn-myStyle'
+                }
             },
-            cancel: {
-                label: 'No',
-                className: 'btn-myStyle'
-            }
-        },
-        callback: function(result) {
-            if(result) {
-                i = $("#head").text();
-                arr[i-1] = "1";
+            callback: function(result) {
+                if(result) {
+                    i = $("#head").text();
+                    arr[i-1] = "1";
                 url = "student.html?state=1"+"&i="+i+"&a=1"+"&arr="+arr;//此处拼接内容
                 window.location.href = url;
-            } 
-        }
+                } 
+            }
+        });
     });
-});
-
-//点击返回按钮
-$(document).ready(function(){
+    //点击返回按钮
     $("#return_s").click(function(e){
         i = $("#head").text();
         window.location.href="student.html?state=1"+"&i="+i+"&a=1"+"&arr="+arr;;
@@ -38,6 +36,7 @@ function back(){
     i = $("#head").text();
     window.location.href="student.html?state=1"+"&i="+i+"&a=1"+"&arr="+arr;
 }
+
 
 $(function(){
     getData();
