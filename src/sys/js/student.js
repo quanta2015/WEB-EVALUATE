0f30a6790510588fbedf21e42327363194b75333
Euodia <38934133+Euodia@users.noreply.github.com>
2018-07-16 17:51:01 +0800

    完成学生作业的详情功能
    测试需要根据最新的schema.sql 更新dotask的数据库并且插入信息

--- src/sys/js/student.js
+++ src/sys/js/student.js
@@ -1,39 +1,58 @@
 
-//按钮点击事件
-$(".Btn_blue").click(function() {
-    i = $(this).attr("name");
-    jump();
-})
-function jump(){
-    url = "homework.html?i="+i+"&s="+s;//此处拼接内容
-    window.location.href = url;
-}
-$(".btn_detail").click(function(){
-
-})
 
 
 $(function(){
-var template = $.templates('#testTmpl');
-
-
-    console.log("search");
-
+     var obj;
      $.ajax({
          url:'../php/task_search.php',
          type:"GET",
          data:'',
+         async:false,
          success: function(data) {
             console.log(data);
-              var obj = JSON.parse(data);
-              console.log(obj);
-               htmlOutput = template.render(obj.data);
+             obj = JSON.parse(data);
+            console.log(obj);
+            var template = $.templates('#testTmpl');
+            htmlOutput = template.render(obj.data);
             $(".box").html(htmlOutput);
+    
 
          }
-     });
 
 });
+
+   $(".btn_detail").click(function(){
+           var i = $(this).attr("name");
+         if( $('#short'+i).attr("style") == "display:none"){
+         $('#short'+i).attr("style","display:inline");
+         $('#detail'+i).attr("style","display:none")}
+         else{
+          $('#short'+i).attr("style","display:none");
+         $('#detail'+i).attr("style","display:inline")}
+
+
+
+         ;})
+
+           $(".Btn_blue").click(function() {
+    i = $(this).attr("name");
+        url = "homework.html?i="+i;//此处拼接内容
+    window.location.href = url;
+     });       //按钮点击事件
+
+/*function jump(){
+    url = "homework.html?i="+is;//此处拼接内容
+    window.location.href = url;
+}
+$(".btn_detail").click(function(){
+
+})*/
+
+
+});
+
+
+
 
 
 
