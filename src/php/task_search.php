446cb5473adc49c2195575ab4519472667c37ea3
Euodia <38934133+Euodia@users.noreply.github.com>
2018-07-16 15:08:55 +0800

    实现学生作业查询从数据库导入
    学生查询任务及完成情况API

--- src/php/task_search.php
+++ src/php/task_search.php
@@ -6,66 +6,60 @@ session_start();
 //获取登录的用户名
 $id= $_SESSION['id'];
 if ($id == "" ) {
-    $response = array(
-         'code' => 20,
-         "msg"=>"empty loginmsg"
-         );
-    $response = CodeUtil::jsons_encode($response);
-    header("Content-Type:text/html;charset=utf-8");
-    die (urldecode(json_encode($response)));
+  $response = array(
+   'code' => 20,
+   "msg"=>"empty loginmsg"
+   );
+  $response = CodeUtil::jsons_encode($response);
+  header("Content-Type:text/html;charset=utf-8");
+  die (urldecode(json_encode($response)));
 }
-
-
-if($_POST['class'] !=''){
-  $CLASS = $_POST['class'];
-}
-
-else $CLASS = $_SESSION['class'];
-
 
 
 if (!$conn) {
-    $response = array(
+  $response = array(
     'code' => 10,
     "msg"=>"connectfail",
     );
-    $response = CodeUtil::jsons_encode($response);
-    header("Content-Type:text/html;charset=utf-8");
-    die (urldecode(json_encode($response)));
+  $response = CodeUtil::jsons_encode($response);
+  header("Content-Type:text/html;charset=utf-8");
+  die (urldecode(json_encode($response)));
 }
 
-else{
-   if($CLASS == '全部' ){
-     $sql4 = "select task_title,publish_date,end_date,user.user_name from task,user where publisher = user.user_number";
-   }
+ if($_SESSION['role']==0){
+    $CLASS = $_POST['class'];
+    if($CLASS == '全部' )
+     $sql4 = "select task_title,publish_date,end_date,user.user_name  from task,user where publisher = user.user_number";
+   else 
+     $sql4 = "select task_title,publish_date,end_date,user.user_name  from task,user where publish_class = '{$CLASS}' and publisher = user.user_number";
+ }
+else {
+  $CLASS = $_SESSION['class'];
+  $sql4 = "select task_title,publish_date,end_date,user.user_name,total_tag from task,user,dotask where publish_class = '{$CLASS}' and user.user_number = publisher and dotask.user_id = '{$id}' and dotask.task_id = task.task_id";
 
-  else{
-       $sql4 = "select task_title,publish_date,end_date,user.user_name from task,user where publish_class = '{$CLASS}' and publisher = user.user_number";}
-
-       $result4 = $conn->query($sql4);
-       header("Content-type: text/html; charset=utf-8");
+}
+  
+   $result4 = $conn->query($sql4);
+   header("Content-type: text/html; charset=utf-8");
        //错误
-       if (0 == mysqli_num_rows($result4)) {
-             $response = array(
-             'code' =>77,
-             "msg"=>"empty"
-              );
-            $response = CodeUtil::jsons_encode($response);
-            mysqli_free_result($result4);
-            header("Content-Type:text/html;charset=utf-8");
-            die (urldecode(json_encode($response)));
-        }
-        //正确返回
-       $rows1 = mysqli_fetch_all($result4,MYSQLI_ASSOC);
-         $response = array(
-             'code' => 0,
-             "msg"=>"success",
-             'data'=>$rows1
-              );
-       $response = CodeUtil::jsons_encode($response);
-       header("Content-Type:text/html;charset=utf-8");
-       echo urldecode(json_encode($response));
-
+   if (0 == mysqli_num_rows($result4)) {
+     $response = array(
+       'code' =>77,
+       "msg"=>"empty"
+       );
+     $response = CodeUtil::jsons_encode($response);
+     mysqli_free_result($result4);
+     header("Content-Type:text/html;charset=utf-8");
+     die (urldecode(json_encode($response)));
    }
- 
-?>
+        //正确返回
+   $rows1 = mysqli_fetch_all($result4,MYSQLI_ASSOC);
+   $response = array(
+     'code' => 0,
+     "msg"=>"success",
+     'data'=>$rows1
+     );
+   $response = CodeUtil::jsons_encode($response);
+   header("Content-Type:text/html;charset=utf-8");
+   echo urldecode(json_encode($response)); 
+ ?>
--- src/sys/js/student.js
+++ src/sys/js/student.js
@@ -1,115 +1,39 @@
-var a = 0;//标志，判断是否为homework提交跳转
-var i = 0;//数组下标
 
-//jsrender渲染
-var arr = [
-{
-    'publisher':'毛婕老师',
-    'content':'做一个温暖的人，快乐并懂得如何快乐。快乐并感染身边的人快乐，尽力做到最好。',
-    'begin': '2018-7-4',
-    'end': '2018-7-10',
-    'state': 0 
-},
-{
-    'publisher':'毛婕老师',
-    'content':'人生只有走出来的美丽，没有等出来的辉煌，天再高又怎样，踮起脚尖就能更接近阳光。',
-    'begin': '2018-7-6',
-    'end': '2018-7-21',
-    'state': 0
-},
-{
-    'publisher':'毛婕老师',
-    'content':'生命中最重要的人，或许当你在身边的时候，能感觉到的也只是淡淡的温暖而已，并不比一杯热茶更显著。但当你失去的时候，整个世界瞬间荒芜。只知道我们总是在战胜空间，却对时间无能为力',
-    'begin': '2018-7-7',
-    'end': '2018-7-21',
-    'state': 0
-},
-{
-    'publisher':'毛婕老师',
-    'content':'人约三月草未莘，间关探搡几回闻，仙槎一叶偏入凡，子衔降珠伴露生，唯有早春最知惜，朱唇贝齿启语箴，正是白紵鸢肩倚，廷风拂扬觅无痕。',
-    'begin': '2018-7-8',
-    'end': '2018-7-21',
-    'state': 0 
+//按钮点击事件
+$(".Btn_blue").click(function() {
+    i = $(this).attr("name");
+    jump();
+})
+function jump(){
+    url = "homework.html?i="+i+"&s="+s;//此处拼接内容
+    window.location.href = url;
 }
-];
+$(".btn_detail").click(function(){
 
+})
+
+
+$(function(){
 var template = $.templates('#testTmpl');
-var htmlOutput = template.render(arr);
-$(".box").html(htmlOutput);
 
 
+    console.log("search");
 
-var s = new Array();//状态数组
-for(var i=0;i<arr.length;++i){
-    s[i] = arr[i].state;
-}
-var arr2 = new Array;//作业内容数组
-for(var j=0;j<arr.length;++j){
-    arr2[j] = arr[j].content;
-}
-$(function(){
-    for(var j=0;j<arr.length;++j){
-        arr[j].content = arr[j].content.substr(0,15);
-        var template = $.templates('#testTmpl');
-        var htmlOutput = template.render(arr);
-        $(".box").html(htmlOutput);
-    }
-    //详情点击事件
-    $(".btn_detail").click(function(){
-        j = $(this).attr("name");
-        alert(arr2[j-1]);
-    });
-    //去完成点击事件
-    $(".Btn_blue").click(function() {
-        i = $(this).attr("name");
-        jump();
-    })
-    function jump(){
-        url = "homework.html?i="+i+"&s="+s;//此处拼接内容
-        window.location.href = url;
-    }
-})
+     $.ajax({
+         url:'../php/task_search.php',
+         type:"GET",
+         data:'',
+         success: function(data) {
+            console.log(data);
+              var obj = JSON.parse(data);
+              console.log(obj);
+               htmlOutput = template.render(obj.data);
+            $(".box").html(htmlOutput);
 
+         }
+     });
 
-
-$(function(){
-    a = $.query.get("a");
-    if(a==1){
-        getData();
-    }
 });
-function getData(){
-    var state = $.query.get("state");
-    var head = $.query.get("i");
-    s = $.query.get("arr");
-    s[head-1] = state;
-    for(var i=0;i<arr.length;++i){
-        arr[i].state = s[(i+1)*2-2];
-    }
-    var template = $.templates('#testTmpl');
-    var htmlOutput = template.render(arr);
-    $(".box").html(htmlOutput);
-    $(".Btn_blue").click(function() {
-        i = $(this).attr("name");
-        url = "homework.html?i="+i+"&s="+s;//此处拼接内容
-        window.location.href = url;
-})
-}
 
-//后端接口
- $().ready(function() {
-   // console.log("search");
 
-    $.ajax({
-        url:'../php/task_search.php',
-        type:"GET",
-        data:'',
-        success: function(data) {
-           console.log(data);
-             var obj = JSON.parse(data);
-             console.log(obj);
-
-        }
-    });
-});
 
