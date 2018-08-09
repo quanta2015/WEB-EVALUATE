fdfe8bb6012236a7808cbd819cd2f6f113a5cb00
Xuan0216 <849651526@qq.com>
2018-08-09 11:04:53 +0800

    update pwdChange

--- src/php/pwdChange.php
+++ src/php/pwdChange.php
@@ -5,12 +5,23 @@ require_once 'common.php';
 header("Content-type: text/html; charset=utf-8");
 session_start();
 
-$id = $_SESSION['id'];
-$newpassword         = $_POST['newpassword'];
-$newpasswordconfirm  = $_POST['newpasswordconfirm'];
+$id                 = $_SESSION['id'];
+$newpassword        = $_POST['newpassword'];
+$newpasswordconfirm = $_POST['newpasswordconfirm'];
 
-if ($newpassword == $newpasswordconfirm) {
-    $sql = "UPDATE user SET user_password = '$newpassword' WHERE user_number = '$id'";
+if (!$conn) {
+    $response = array(
+        'code' => 55,
+        "msg"  => "connectfail"
+    );
+    $response = CodeUtil::jsons_encode($response);
+    header("Content-Type:text/html;charset=utf-8");
+    die(urldecode(json_encode($response)));
+} 
+
+else if ($newpassword == $newpasswordconfirm) {
+    $sql    = "UPDATE user SET user_password = '$newpassword
+                          ' WHERE user_number = '$id'";
     $result = mysqli_query($conn, $sql);
 
     if ($result) { //   密码修改成功
@@ -20,8 +31,9 @@ if ($newpassword == $newpasswordconfirm) {
         );
         $response = CodeUtil::jsons_encode($response);
         echo urldecode(json_encode($response));
-    }
-    else{//  密码修改失败
+    } 
+
+    else { //  密码修改失败
         $response = array(
             'code' => 99,
             "msg"  => "updatefail"
@@ -30,13 +42,16 @@ if ($newpassword == $newpasswordconfirm) {
         echo urldecode(json_encode($response));
     }
 } 
+
 else { //   两次密码不一致
     $response = array(
-            'code' => 1,
-            "msg"  => "confirmfail"
-        );
-        $response = CodeUtil::jsons_encode($response);
-        echo urldecode(json_encode($response));
+        'code' => 1,
+        "msg"  => "confirmfail"
+    );
+    $response = CodeUtil::jsons_encode($response);
+    echo urldecode(json_encode($response));
 }
+
+mysqli_close($conn);
 
 ?>
--- src/sys/js/pwdChange.js
+++ src/sys/js/pwdChange.js
@@ -1,25 +1,33 @@
-function get(){//学生
-	var p1 = document.getElementById("password").value;
-	var p2 = document.getElementById("confirm_password").value;
-	if(p1 == p2){
-		alert("修改成功");
-        window.location.href = "student.html";
-	}
-	else{
-		alert("两次密码不一致");
-		window.location.href = "pwdChange.html";
-	}
+function get(){
+    var p1 = document.getElementById("password").value;
+    var p2 = document.getElementById("confirm_password").value;
+    if(p1 == p2){
+        alert("修改成功");
+    }
+    else{
+        alert("两次密码不一致");
+    }
 }
 
-function getT(){//老师
-	var p1 = document.getElementById("password").value;
-	var p2 = document.getElementById("confirm_password").value;
-	if(p1 == p2){
-		alert("修改成功");
-        window.location.href = "task.html";
-	}
-	else{
-		alert("两次密码不一致");
-		window.location.href = "pwdChangeT.html";
-	}
-}
+$("#submit").click(function () {
+
+    $.ajax({
+        url: '../../php/pwdChange.php',
+        type: "POST",
+        data: $('#content-main').serialize(),
+
+        success: function (data) {
+            console.log(data);
+            var obj = JSON.parse(data);
+            if (obj.code == 0) {
+                toastr.success('密码修改成功');
+            } else if (obj.code == 99) {
+                toastr.warning('密码修改失败');
+            } else if (obj.code == 1) {
+                toastr.waring('两次密码不一致');
+            } else if (obj.code == 55){
+                toastr.error('数据库连接失败');
+            }
+        }
+    });
+});
