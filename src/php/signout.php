1889d7a48162d4a0031347dba230ee0a292d0d15
Euodia <38934133+Euodia@users.noreply.github.com>
2018-09-12 21:13:40 +0800

    注销按钮
    1

---
+++ src/php/signout.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'common.php';
+header("Content-type: text/html; charset=utf-8");
+session_start();
+
+
+    $_SESSION['id'] = NULL;
+    $_SESSION['class'] = NULL;
+    $_SESSION['role'] = NULL;
+    $_SESSION['name'] = NULL;
+
+
+if( $_SESSION['id'] == NULL &&  $_SESSION['class'] == NULL && $_SESSION['class'] == NULL &&  $_SESSION['name'] == NULL){
+
+      $response = array(
+             'code' => 0,
+             "msg"=>"success"
+              );
+
+            $response = CodeUtil::jsons_encode($response);
+            header("Content-Type:text/html;charset=utf-8");
+            echo urldecode(json_encode($response));
+
+
+}
+
+
+else {  $response = array(
+             'code' => 70,
+             "msg"=>"fail"
+
+              );
+            $response = CodeUtil::jsons_encode($response);
+            header("Content-Type:text/html;charset=utf-8");
+            echo urldecode(json_encode($response));}
