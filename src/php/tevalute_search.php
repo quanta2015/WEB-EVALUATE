5319c585b28f48538f186c3ea76ce3988ec51356
Euodia <38934133+Euodia@users.noreply.github.com>
2018-09-29 23:40:29 +0800

    update

--- src/php/tevalute_search.php
+++ src/php/tevalute_search.php
@@ -4,11 +4,8 @@ require_once 'conn.php';
 require_once 'common.php';
 
 
-//$id = $_SESSION["id"];
-//$role = $_SESSION['role'];
-
-$id = 7;
-$role = 1;
+$id = $_SESSION["id"];
+$role = $_POST['role'];
 
 
   $sql = "select task.task_id,task.task_title,totalgrade.*,user.user_name,user.user_number,user.user_class,dotask.total_tag from dotask,totalgrade,user,task where task.task_id = dotask.task_id and totalgrade.dotask_id = dotask.id and user.user_number = dotask.user_id and totalgrade.role = {$role} and totalgrade.evalute_user = {$id}";
