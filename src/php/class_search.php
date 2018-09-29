fb0c96eee05630370f895654023dc766e3013643
Euodia <38934133+Euodia@users.noreply.github.com>
2018-09-29 23:38:38 +0800

    Revert "update"
    This reverts commit 848916b0cae8eac3bb3c19a065af93a74f637e4c.

--- src/php/class_search.php
+++ src/php/class_search.php
@@ -7,7 +7,7 @@ LoginCheck::checklogin();
 $id = $_SESSION['id'];
 if (!$conn) connfail();
 
-  $sql = "select  class_name from class where teacher_num = {$id}";
+  $sql = "select class_name from class where teacher_num = {$id}";
   $result = $conn->query($sql);
 
  if (0 == mysqli_num_rows($result))existempty();
--- src/php/tevalute_search.php
+++ src/php/tevalute_search.php
@@ -4,8 +4,11 @@ require_once 'conn.php';
 require_once 'common.php';
 
 
-$id = $_SESSION["id"];
-$role = $_POST['role'];
+//$id = $_SESSION["id"];
+//$role = $_SESSION['role'];
+
+$id = 7;
+$role = 1;
 
 
   $sql = "select task.task_id,task.task_title,totalgrade.*,user.user_name,user.user_number,user.user_class,dotask.total_tag from dotask,totalgrade,user,task where task.task_id = dotask.task_id and totalgrade.dotask_id = dotask.id and user.user_number = dotask.user_id and totalgrade.role = {$role} and totalgrade.evalute_user = {$id}";
