cbea78c3b00b2816256197c57432647857d61cd5
lc <631674132@qq.com>
2018-10-08 02:12:02 +0800

    上传可以了吧

--- src/php/system.php
+++ src/php/system.php
@@ -1,8 +1,35 @@
 <?php 
 //echo phpinfo(); //查看php.ini所在位置
-$path = "../../upload.php";
+require_once 'conn.php';
+require_once 'common.php';
+$path = "../upload.php";
 $filename = "hahaha.doc";
  $newPath= $path.'/'.$filename;
 echo $newPath.'<br/>';
-echo "hhh";
+echo "hhh<br/>";
+$newpath = array("u大发","upload大法师.php","upload.p的放大法hp");
+  $newPath1 = $newPath[0];
+    $newPath2 = $newPath[1];
+    $newPath3 = $newPath[2];
+
+$id = 061;
+
+if (!$conn)
+   echo "aaaa";
+
+    $sql = "UPDATE dotask SET doc_url = '$newPath[0]' where user_id = '47' and task_id = '102'";
+   $result = mysqli_query($conn, $sql);
+   $sql2 = "UPDATE dotask SET doc_url ='$newPath1',ppt_url = '$newPath2',video_url = '$newPath3' WHERE user_id = '$id' and task_id = '102'";
+
+   $result2 = $conn->query($sql2);
+   $sql3 = "select * from user";
+   $result3 = $conn->query($sql3);
+
+   if($result2){
+   	    echo "<script>alert('上传成功');</script>";
+     header("Refresh:1;url=../pages/homework.html");
+   }
+   else{
+   	   echo "not ok";
+   }
 ?>
--- src/php/upload.php
+++ src/php/upload.php
@@ -1,16 +1,15 @@
 <?php
-include 'conn.php';
-include 'common.php';
-
+require_once 'conn.php';
+require_once 'common.php';
 $id = $_SESSION["id"];
-
-//允许上传的文件格式
-$allowtype = array("doc", "docx", "pdf", "ppt", "pptx", "mp4", "avi","flv", "wmv","mov","zip","rar","7z"); 
-
+echo $id;
+$allowtype = array("doc", "docx", "pdf", "ppt", "pptx", "mp4", "avi","flv", "wmv","mov","zip","rar","7z");  //允许上传的文件格式
 $size = 100000000; //设置允许大小100M以内的文件
-$path = "../../upload"; //设置上传后保存文件的路径
-$success = 0; //记录成功的次数
-$newPath = array(); //上传文件的放置路径
+$path = "./upload"; //设置上传后保存文件的路径
+$success = 0;
+$newPath1;
+$newPath2;
+$newPath3;
 
 //判断文件是否可以上传到服务器 $_FILES['myfile'][error]为0表示成功
 //循环
@@ -18,32 +17,31 @@ for( $i = 0;$i < count($_FILES['file']['error']);$i++ ){
 
     $upfile[$i] = $_FILES['file']['name'][$i];
 
-    if($_FILES['file']['error'][$i]>0){
+   if($_FILES['file']['error'][$i]>0){
 
         echo "上传错误";
         switch($_FILES['file']['error'][$i]){
         
-            case 1: 
-                   die('第'.($i+1).'个文件上传文件大小超出了PHP配置文件中的约定值：upload_max_filesize');
-                    //ero('27','第'{$i+1}'个文件上传文件大小超出了PHP配置文件中的约定值');
-                    //exit();
-                    break;
-            case 2: die('上传第'.($i+1).'个文件大小超出了表单中的约定值：MAX_FILE_SIZE');
-                    //ero('30','上传第'($i+1)'个文件大小超出了表单中的约定值：MAX_FILE_SIZE');
-                    //exit();
-                    break;
-            case 3: die('第'.($i+1).'个文件只被部分上传');
-                    //ero('33','第'($i+1)'个文件只被部分上传');
-                    //exit();
-                    break;
-            case 4: die('第'.($i+1).'个文件没有上传');
-                    //ero('36','第'($i+1)'个文件没有上传');
-                    //exit();
-                    break;
-            default: die('未知错误');
-                     ero('39','未知错误');
-                     //exit();
-                     break;
+            case 1:                    
+                    ero(27,"文件上传文件大小超出了PHP配置文件中的约定值");
+                    exit();
+                  
+            case 2: 
+                    ero(30,'文件大小超出了表单中的约定值：MAX_FILE_SIZE');
+                    exit();
+                
+            case 3: 
+                    ero(33,'文件只被部分上传');
+                    exit();
+                  
+            case 4: 
+                    ero(35,'文件没有上传');
+                    exit();
+                    
+            default: 
+                     ero(39,'未知错误');
+                     exit();
+                    
         }
     }
 
@@ -52,26 +50,19 @@ for( $i = 0;$i < count($_FILES['file']['error']);$i++ ){
     //判断上传的文件是否为允许的文件类型，通过文件的后缀名
     //array_pop 弹出并返回数组中的最后一个元素，并将array的长度减1
     $file = explode(".",$_FILES['file']['name'][$i]);
-    var_dump($file);
     $hz[$i] = array_pop($file);
-    print_r($hz[$i]);
-    if(!in_array(strtolower($hz[$i]),$allowtype)){
+    if(!in_array($hz[$i],$allowtype)){
 
-        die("第".($i+1)."个文件后缀是<b>{$hz}</b>,不是允许的文件类型！");
+        ero(53,"不是允许的文件类型！");
+        exit();
     }
 
- /*
-    list($maintype,$subtype) = explode("/",$_FILES['myfile']['type']);
-    if($maintype == "text"){
-
-        die("不能上传文本文件");
-    }
-    */
+  
 
     //判断上传的文件是否允许大小
     if($_FILES['file']['size'][$i]>$size){
 
-        die("第".($i+1)."个文件超过了允许的<b>{$size}</b>");
+        ero(68,"文件超过了允许的大小");
     }
 
 
@@ -84,38 +75,43 @@ for( $i = 0;$i < count($_FILES['file']['error']);$i++ ){
 
         if(!move_uploaded_file($_FILES['file']['tmp_name'][$i],$path.'/'.$filename[$i])){
         
-            die("不能将文件移动到指定目录");
+            ero(76,"不能将文件移动到指定目录");
         }
-    }else{
+    }
+    else{
 
-        die("上传文件{$_FILES['file']['name'][$i]}不是一个合法文件");
+        ero(86,"上传文件不是一个合法文件");
     }
 
     //如果文件上传成功
-    $filesize[$i] = $_FILES['file']['size'][$i]/1024;
     $success++;
+    $filesize[$i] = $_FILES['file']['size'][$i]/1024;
+    echo "文件{$upfile[$i]}上传成功，保存在目录{$path}中，文件大小为{$filesize[$i]}KB<br>";
     $newPath[$i] = $path.'/'.$filename[$i];
+    
     //echo $newPath.'<br/>';
 }
 
-if($success ==3){
-    $sql = "INSERT into 'dotask' (doc_url,ppt_url,vedio_url) VALUES ('{$newpath[0]}','{$newPath[1]}','{$newPath[2]}') where id = '{$id}'";
-   $result = mysqli_query($conn, $sql2);
-   if(1==$result2){
+if($success == 3){
+    $newPath1 = $newPath[0];
+    $newPath2 = $newPath[1];
+    $newPath3 = $newPath[2];
+    $sql = "UPDATE dotask SET doc_url ='{$newPath1}',ppt_url = '{$newPath2}',video_url = '{$newPath3}' WHERE user_id = '{$id}'";
+   $result = mysqli_query($conn, $sql);
+
+   if(1 == $result){
        success('');
-       alert("上传成功！")；
-       echo "<script>windows.location.href='../html/student.html';</script>";
+       echo "<script>alert('上传成功');</script>";
+       header("Refresh:1;url=../pages/homework.html");
    }
    else{ 
-       ero('20','unkonwfail');
+       echo "<script>alert('上传失败');</script>";
+       ero('88','入库失败！');
+       //exit();
    }
-   /*  if(0 == mysqli_num_rows($result)) {
-       existempty();
-    }
-
-    else{
-        $row1 = mysqli_fetch_all($result,MYSQLI_ASSOC);
-        success($row1);
-    }
-    */
+}
+else{
+   echo "<script>alert('上传失败');</script>";
+   ero('20','unkonwfail');
+   //exit();
 }
