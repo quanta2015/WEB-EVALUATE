5c9e898b7afd1820e0b23ce642a0d0a1fccbeb8c
陶娣 <454101322@qq.com>
2018-10-10 11:57:14 +0800

    update

--- src/sys/js/gradeSearch.js
+++ src/sys/js/gradeSearch.js
@@ -1,125 +1,3 @@
-// var arr = [
-// {
-//     'task_title':'作业1',
-//     'user_num':'1',
-//     'user_class':'软工162',
-//     'user_name':'陶娣',
-//     'selfGrade':98,
-//     'groupGrade':85,
-//     'teacherGrade':90,
-//     'totalGrade':90
-// },
-// {
-//     'task_title':'作业2',
-//     'user_num':'1',
-//     'user_class':'软工162',
-//     'user_name':'陶娣',
-//     'selfGrade':98,
-//     'groupGrade':85,
-//     'teacherGrade':90,
-//     'totalGrade':90
-// },
-// {
-//     'task_title':'作业1',
-//     'user_num':'2',
-//     'user_class':'软工161',
-//     'user_name':'叶艳洁',
-//     'selfGrade':98,
-//     'groupGrade':85,
-//     'teacherGrade':90,
-//     'totalGrade':90
-// },
-// {
-//     'task_title':'作业2',
-//     'user_num':'2',
-//     'user_class':'软工161',
-//     'user_name':'叶艳洁',
-//     'selfGrade':"",
-//     'groupGrade':"",
-//     'teacherGrade':90,
-//     'totalGrade':""
-// },
-// ]
-// //获取模版
-// var jsRenderTpl = $.templates('#theTmpl');
-// //模版与数据结合
-// var finalTpl = jsRenderTpl(arr);
-// $('.box').html(finalTpl);
-
-
-
-//  var classes, homeworks; //返回的班级信息存储在classes，对应班级作业在homeworks
-//  $(document).ready(function() {
-//     //查找老师所有的班级
-//      $.ajax({
-//            url: '../php/class_search.php',
-//              type: "POST",
-//             async: false,
-//              success: function(data) {
-
-//                 classes = JSON.parse(data);
-//                 console.log(classes);
-//                  var context;
-//                 if (classes.code == 0) {
-//                     for (var i = 0; i < classes.data.length; i++)
-//                        context = context + "<option>" + classes.data[i].class_name + "</option>";
-//                     context = context + "<option>全部</option>";
-//                    $('#classSlct').html(context);
-//                 } else if (classes.code == 77) $('#classSlct').html("没有班级");
-
-
-//              }
-
-//          }),
-//          //查找指定班级的所有作业
-//         $.ajax({
-//              url: '../php/task_search.php',
-//              type: "POST",
-//             async: false,
-//             data: { class: $("#classSlct").val() },
-//              success: function(data) {
-
-//                 homeworks = JSON.parse(data);
-//                  console.log(homeworks);
-//                 //渲染作业列表
-//                  if (homeworks.code == 0) {
-//                      var context;
-//                      for (var i = 0; i < homeworks.data.length; i++)
-//                         context = context + "<option>" + homeworks.data[i].task_title + "</option>";
-//                    context = context + "<option>全部</option>";
-//                     $('#taskSlct').html(context);
-//                  } else if (homeworks.code == 77) $('#taskSlct').html("<option>没有作业</option>");
-
-//             }
-//         });
-
-//     $('#classSlct').change(function() {
-//          $.ajax({
-//              url: '../php/task_search.php',
-//             type: "POST",
-//             async: false,
-//             data: { class: $("#classSlct").val() },
-//             success: function(data) {
-//                  homeworks = JSON.parse(data);
-//                 console.log(homeworks);
-//                  if (homeworks.code == 0) {
-//                      var context;
-//                     for (var i = 0; i < homeworks.data.length; i++)
-//                      context = context + "<option>" + homeworks.data[i].task_title + "</option>";
-//                     context = context + "<option>全部</option>";
-//                      $('#taskSlct').html(context);
-//                } else if (homeworks.code == 77) $('#taskSlct').html("<option>没有作业</option>");
-
-//             }
-
-
-//          });
-
-//      })
-
-
-//  })
-
 function cancel() {
     $.ajax({
         url: '../php/signout.php',
@@ -134,9 +12,7 @@ function cancel() {
             };
 
         }
-
     })
-
 }
 
 
@@ -147,12 +23,17 @@ $(document).ready(function() {
         url: '../php/final_search.php',
         type: 'get',
         async: false,
-        data: {role: "1"},
+        data: { role: "1" },
         success: function(data) {
             //获取学生数据
             console.log(data);
             stdObj = JSON.parse(data);
             console.log(stdObj.data);
+
+            for (var i = 0; i < stdObj.data.length; i++) {
+                var result = stdObj.data[i].student * stdObj.data[i].s_percent / 100 + stdObj.data[i].teacher * stdObj.data[i].t_percent / 100 + stdObj.data[i].group * stdObj.data[i].g_percent / 100;
+                stdObj.data[i].totalGrade = result;
+            }
             //获取模版
             var jsRenderTpl = $.templates('#theTmpl');
             //模版与数据结合
@@ -193,8 +74,8 @@ $(document).ready(function() {
 //二级联动
 $("#classSlct").change(function() {
     var context;
-    for(var i = 0; i < homeworks.data.length; i++) {
-        if(homeworks.data[i].publish_class == $("#classSlct").val()){
+    for (var i = 0; i < homeworks.data.length; i++) {
+        if (homeworks.data[i].publish_class == $("#classSlct").val()) {
             context = context + "<option>" + homeworks.data[i].task_title + "</option>";
         }
     }
