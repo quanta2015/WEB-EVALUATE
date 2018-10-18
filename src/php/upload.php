6f5fba73cd38b96eb98b1d2ab83f90006890115d
Julia <849651526@qq.com>
2018-10-18 09:52:37 +0800

    up up

--- src/php/upload.php
+++ src/php/upload.php
@@ -2,127 +2,56 @@
 require_once 'conn.php';
 require_once 'common.php';
 $id = $_SESSION["id"];
-echo $id."<br/>";
+echo $id . "<br/>";
 $dotask_id = $POST['dotask_id'];
-echo $dotask_id."hhhhh"."<br/>";
-$size = 100000000; //设置允许大小100M以内的文件
-$path = "./homework"; //设置上传后保存文件的路径
+// echo $dotask_id . "hhhhh" . "<br/>";
+$size    = 100000000; //设置允许大小100M以内的文件
+$path    = "./upload"; //设置上传后保存文件的路径
 $success = 0;
-    if(IS_AJAX)
-    {
-        $postfix = '';
-        foreach($_FILES['file']['tmp_name'] as$k=>$v)
-        {
-            //获取文件名后缀
-            $file = explode(".", $_FILES['file']['name'][$k]);
-            $postfix = array_pop($file);
+if (IS_AJAX) {
+    $postfix = '';
+    foreach ($_FILES['file']['tmp_name'] as $k => $v) {
+        //获取文件名后缀
+        $file    = explode(".", $_FILES['file']['name'][$k]);
+        $postfix = array_pop($file);
 
-            //判断文件类型是否正确
-        
-            //文件名转码
-            $name = iconv("UTF-8", "gb2312", $_FILES['file']['name'][$k]);
+        //判断文件类型是否正确
 
-            //保存文件
-            move_uploaded_file($v,$path.'/'.$name); //文件名，地址
-               
-            echo $k." ".$_FILES['file']['name'][$k]." ".$postfix." ";
+        //文件名转码
+        $name = iconv("UTF-8", "gb2312", $_FILES['file']['name'][$k]);
 
-            
-        }
-        success($dotask_id);
+        //保存文件
+        move_uploaded_file($v, $path . '/' . $name); //文件名，地址
 
-    }
-    else{
-        echo "<script>alert('上传失败');</script>";
-    }
-  
-
-
-
-/*
-//判断文件是否可以上传到服务器 
-//循环
-for ($i = 0; $i < count($_FILES['file']['error']); $i++) {
-    $upfile[$i] = $_FILES['file']['name'][$i];
-    if ($_FILES['file']['error'][$i] > 0) {
-        echo "上传错误";
-        switch ($_FILES['file']['error'][$i]) {
-            case 1:
-                ero(27, "文件上传文件大小超出了PHP配置文件中的约定值");
-                exit();
-            case 2:
-                ero(30, '文件大小超出了表单中的约定值：MAX_FILE_SIZE');
-                exit();
-            case 3:
-                ero(33, '文件只被部分上传');
-                exit();
-            case 4:
-                ero(35, '文件没有上传');
-                exit();
+        echo $k . " " . $_FILES['file']['name'][$k] . "<br/>";
+        $newPath = $path . '/' . $_FILES['file']['name'][$k];
+        echo $newPath . "<br/>";
+        switch ($k) {
+            case '0':
+                $sql = "UPDATE dotask SET doc_url ='{$newPath}' WHERE user_id = '{$id}'";
+                break;
+            case '1':
+                $sql = "UPDATE dotask SET ppt_url = '{$newPath}' WHERE user_id = '{$id}'";
+                break;
+            case '2':
+                $sql = "UPDATE dotask SET video_url = '$newPath' WHERE user_id = '{$id}'";
+                break;
             default:
-                ero(39, '未知错误');
-                exit();
+                break;
         }
     }
-    //判断上传的文件是否为允许的文件类型，通过文件的后缀名
-    //array_pop 弹出并返回数组中的最后一个元素，并将array的长度减1
-    $file = explode(".", $_FILES['file']['name'][$i]);
-    $hz[$i] = array_pop($file);
-    if (!in_array($hz[$i], $allowtype)) {
-        ero(53, "不是允许的文件类型！");
-        exit();
-    }
-    //判断上传的文件是否允许大小
-    if ($_FILES['file']['size'][$i] > $size) {
-        ero(68, "文件超过了允许的大小");
-    }
-
-    //学号加姓名加作业号
-    $filename[$i] = $id."+".$name. "+" $task_id. ".".$hz[$i];
-
-
-    //判断是否为上传文件
-    if (is_uploaded_file($_FILES['file']['tmp_name'][$i])) {
-        if (!move_uploaded_file($_FILES['file']['tmp_name'][$i], $path . '/' . $filename[$i])) {
-            ero(76, "不能将文件移动到指定目录");
-        }
-    } else {
-        ero(86, "上传文件不是一个合法文件");
-    }
-    //如果文件上传成功
-    $success++;
-    $filesize[$i] = $_FILES['file']['size'][$i] / 1024;
-    echo "文件{$upfile[$i]}上传成功，保存在目录{$path}中，文件大小为{$filesize[$i]}KB<br>";
-    $newPath[$i] = $path . '/' . $filename[$i];
-    //echo $newPath.'<br/>';
-    
-}
-
-
-if ($success == 3) {
-    $newPath1 = $newPath[0];
-    $newPath2 = $newPath[1];
-    $newPath3 = $newPath[2];
-    $sql = "UPDATE dotask SET doc_url ='{$newPath1}',ppt_url = '{$newPath2}',video_url = '{$newPath3}' WHERE user_id = '{$id}'";
     $result = mysqli_query($conn, $sql);
     if (1 == $result) {
         success('');
-        echo "<script>alert('上传成功');</script>";
-         header("Refresh:1;url=../pages/homework.html");
-        
-    } else {
-        echo "<script>alert('上传失败');</script>";
-        ero(88, '入库失败！');
-          header("Refresh:1;url=../pages/homework.html");
-        //exit();
-        
+    } 
+    else {
+        ero(99, 'fail');
     }
-} else {
-    ero(115, 'unkonwfail');
-    echo "<script>alert('上传失败');</script>";
-    header("Refresh:1;url=../pages/homework.html");
-    //exit();
-    
 }
 
-*/
+/*
+
+//学号加姓名加作业号
+$filename[$i] = $id."+".$name. "+" $task_id. ".".$hz[$i];
+
+ */
