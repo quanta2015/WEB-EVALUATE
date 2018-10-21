6ae7fd1df7785eec9762276b1492e1111634525b
YOEmma <38882725+YOEmma@users.noreply.github.com>
2018-10-21 10:28:51 +0800

    Update classManagement.js

--- src/sys/js/classManagement.js
+++ src/sys/js/classManagement.js
@@ -1,20 +1,32 @@
     //check框选中数量
     var ckNum = 0;
-    //定义数组
-    var clsInfor = [{
-        name: "软工161"
-        }, {
-
-        name: "软工162"
-        }, {
-        name: "物联网162"
-        }];
-    var html = $("#clsTmpl").render(clsInfor);
-    $("#list").append(html);
-    //全选
     var isCheckAll = false;
-    //     //悬浮框的位置
-    toastr.options.positionClass = 'toast-top-left';
+    //  var clsNm = [];
+    //  var clsId = [];
+    var selcId = [];
+    toastr.options.positionClass = 'toastr-top-left';
+
+
+    $(document).ready(function () {
+
+        $.ajax({
+            url: '../php/class_search.php',
+            type: 'get',
+            async: false,
+            success: function (data) {
+
+                classes = JSON.parse(data);
+                console.log(classes);
+                var i;
+                var html = $("#clsTmpl").render(classes.data);
+                $("#list").append(html);
+
+            }
+
+        });
+    });
+
+
 
 
     function clsAdd() {
@@ -35,15 +47,30 @@
             callback: function (result) {
                 console.log(result);
                 if (result) {
-                    clsInfor.push({
-                        name: result
-                    });
-                    for (i = 0, tmp = clsInfor.length; i < tmp; i++) {
-                        $("#cls-items").remove();
-                    }
+                    $.ajax({
+                        url: '../php/class_set.php',
+                        type: "POST",
+                        data: {
+                            do: '0',
+                            class_name: result,
+                        },
+                        success: function (data) {
+                            //                            console.log(data);
+                            //                            clsNm.push({
+                            //                                name: result
+                            //                            });
+                            //                            clsId.push({
+                            //                                id: clsId.length
+                            //                            })
+                            for (i = 0, tmp = classes.data.length; i < tmp; i++) {
+                                $("#cls-items").remove();
+                            }
+                            var html = $("#clsTmpl").render(classes.data);
+                            $("#list").append(html);
 
-                    var html = $("#clsTmpl").render(clsInfor);
-                    $("#list").append(html);
+                        }
+                    })
+
 
                 }
             }
@@ -65,7 +92,7 @@
     }
     //多选删除
     function clsMulRemove() {
-        if (clsInfor.length) {
+        if (classes.data.length) {
             if (isCheckAll) {
                 bootbox.confirm({
                     message: "确认删除全部数据吗?",
@@ -80,13 +107,25 @@
                         }
                     },
                     callback: function (result) {
+
                         if (!result) {
                             toastr.success('已成功取消');
 
                         } else {
-                            $("div#cls-items").remove();
-                            clsInfor.splice(0, clsInfor.length);
-                            toastr.success('已成功删除');
+                            $.ajax({
+                                url: '../php/class_set.php',
+                                type: "POST",
+                                data: {
+                                    do: '1',
+                                    class_id: 1
+                                },
+                                success: function () {
+                                    $("#cls-items").remove();
+                                    //  clsNm.splice(0, clsNm.length);
+                                    //  clsId.splice(0, clsId.length);
+                                    toastr.success('已成功删除');
+                                }
+                            })
                         }
                     }
 
@@ -118,18 +157,41 @@
                                 toastr.success('已成功取消');
 
                             } else {
-                                num = 0;
+                                var num = 0;
+                                var j = 0;
                                 $("input[type='checkbox']").each(function () {
                                     if (this.checked) {
                                         tmp = $(this).parent().next().text();
-                                        clsInfor.splice(tmp - num - 1, 1);
+                                        selcId.push(tmp);
+
+                                        //  clsNm.splice(tmp - num - 1, 1);
+                                        // selcId[j] = clsId.splice(tmp - num - 1, 1);
                                         num++;
+                                        // j++;
                                     }
                                 });
-                                $("div#cls-items").remove();
-                                toastr.success('已成功删除');
-                                html = $("#clsTmpl").render(clsInfor);
-                                $("#list").append(html);
+
+                                $.ajax({
+                                    url: '../php/class_set.php',
+                                    type: "POST",
+                                    data: {
+                                        do: 1,
+                                        class_id: selcId
+                                    },
+                                    success: function (data) {
+                                        console.log(selcId);
+                                        console.log(data);
+                                        $("div#cls-items").remove();
+                                        toastr.success('已成功删除');
+
+                                        html = $("#clsTmpl").render(classes.data);
+                                        $("#list").append(html);
+                                        //  selcId.splice(0, selcId.length);
+
+                                    }
+                                })
+
+
 
                             }
                         }
@@ -143,6 +205,7 @@
     }
 
     function clsRemove(n) {
+
         bootbox.confirm({
             message: "确认删除此班级吗?",
             buttons: {
@@ -160,35 +223,30 @@
                     toastr.success('已成功取消');
 
                 } else {
+                    $.ajax({
+                        url: '../php/class_set.php',
+                        type: "POST",
+                        data: {
+                            dp: '1',
+                            class_id: n
+                        },
+                        success: function (data) {
+                            console.log(data);
+                            if (data.code == 0) {
+                                $("div#cls-items").remove();
+                                toastr.success('已成功删除');
+                                html = $("#clsTmpl").render(classes.data);
+                                $("#list").append(html);
+                            } else
+                                toastr.success('删除失败');
 
-                    $("div#cls-items").remove();
-                    clsInfor.splice(n - 1, 1);
-                    toastr.success('已成功删除');
-                    html = $("#clsTmpl").render(clsInfor);
-                    $("#list").append(html);
+
+                        }
+                    })
+
                 }
             }
 
-        });
-
-
-    }
-
-    function cancel() {
-        $.ajax({
-            url: '../php/signout.php',
-            type: "POST",
-            success: function (data) {
-                result = JSON.parse(data);
-                if (result.code == 0) {
-                    alert("退出成功");
-                    window.location.href = "../index.html";
-                } else {
-                    alert("退出失败！");
-                };
-
-            }
 
         })
-
     }
