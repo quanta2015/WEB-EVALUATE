25fad95517f36797baa959131dc707d9849a4805
Euodia <38934133+Euodia@users.noreply.github.com>
2018-10-22 21:22:58 +0800

    1
    1

---
+++ src/php/groupeval_search.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'conn.php';
+require_once 'common.php';
+
+//获取登录的用户名
+LoginCheck::checklogin();
+$dotask_id= $_SESSION['dotask_id'];
+
+if($role ==1)
+	ero(22,$_SESSION['class']);
+
+if (!$conn) connfail();
+
+  $sql = "select distinct totalgrade.*,user.user_name from `totalgrade`,`user` where totalgrade.dotask_id = {$dotask_id} and totalgrade.role =3 and user.user_number = totalgrade.evalute_user";
+  $result = $conn->query($sql);
+
+ if (0 == mysqli_num_rows($result))existempty();
+  $rows = mysqli_fetch_all($result,MYSQLI_ASSOC);
+  success($rows);
+
+?>
