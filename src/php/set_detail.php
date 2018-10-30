fb41cc944bd3cb3aab9496c8c9812c2dc5a282b1
Euodia <38934133+Euodia@users.noreply.github.com>
2018-10-30 22:03:37 +0800

    update

--- src/php/set_detail.php
+++ src/php/set_detail.php
@@ -2,23 +2,28 @@
 require_once 'conn.php';
 require_once 'common.php';
 
-$id = $POST['id'];
-$opr = $POST['operate'];
-$content = $POST['$content'];
+$id = $_POST['id'];
+$opr = $_POST['operate'];
+$cc = $_POST['content'];
 
+$content = json_decode($cc,TRUE);
+
+//echo "opris:".$content["item_title"];
 switch($opr){
-	case 1: $sql = "UPDATE `evaluate` SET `item_title`= {$content["item_title"]},`item_content`={$content["item_content"]},`item_point`={$content["item_point"]} WHERE id = {$id}";
+	case 1: $sql = "UPDATE `evaluate` SET `item_title`= '{$content["item_title"]}',`item_content`='{$content["item_content"]}',`item_point`='{$content["item_point"]}' WHERE id = '{$id}'";
 	break;
 	case 2:$sql = "INSERT INTO `evaluate`(`tid`, `table_name`, `item_title`, `item_content`, `item_point`) VALUES ({$content["tid"]},{$content["table_name"]},{$content["item_title"]},{$content["item_content"]},{$content["item_point"]})";
 	break;
 	case 3:$sql = "DELETE FROM `evaluate` WHERE id = {$id}";
 }
 
+//echo $sql;
 if (!$conn) connfail();
 
   $result = $conn->query($sql);
+  $msg = mysqli_error($conn);
 
- if (0 == mysqli_num_rows($result))ero('20','unknow');
+if($result == 0 ) ero(20,$msg);
 
   success('');
 
--- src/sys/js/set.js
+++ src/sys/js/set.js
@@ -25,143 +25,112 @@ function setInit(data) {
     }
     var jsRenderTpl = $.templates('#theTmpl1');
     var finalTpl1 = jsRenderTpl(arr1);
-    console.log(arr1);
+    document.getElementById("tab1").title = arr1[0].table_name;
+    // console.log(arr1);
     $('.box1').html(finalTpl1);
 
 
     var jsRenderTpl = $.templates('#theTmpl2');
     var finalTpl2 = jsRenderTpl(arr2);
-    console.log(arr2);
+    // console.log(arr2);
     $('.box2').html(finalTpl2);
 
     var jsRenderTpl = $.templates('#theTmpl3');
     var finalTpl3 = jsRenderTpl(arr3);
-    console.log(arr3);
+    // console.log(arr3);
     $('.box3').html(finalTpl3);
 
     var jsRenderTpl = $.templates('#theTmpl4');
     var finalTpl4 = jsRenderTpl(arr4);
-    console.log(arr4);
+    // console.log(arr4);
     $('.box4').html(finalTpl4);
+}
+
+function createli() {
+
 }
 
 
 
 $(document).ready(function() {
-    console.log(1);
     $.ajax({
         url: '../php/detail_search.php',
         type: 'get',
         // async: false,
         success: function(data) {
-            console.log(data);
-            changeArr.splice(0, changeArr.length);
-    evastandard.splice(0, changeArr.length);
-    evastandard = JSON.parse(data);
-    arr1.splice(0, arr1.length);
-    arr2.splice(0, arr2.length);
-    arr3.splice(0, arr3.length);
-    arr4.splice(0, arr4.length);
-    for (var i = 0; i < evastandard.data.length; i++) {
-        if (evastandard.data[i].tid == 1) {
-            arr1.push(evastandard.data[i]);
-        }
-        if (evastandard.data[i].tid == 2) {
-            arr2.push(evastandard.data[i]);
-        }
-        if (evastandard.data[i].tid == 3) {
-            arr3.push(evastandard.data[i]);
-        }
-        if (evastandard.data[i].tid == 4) {
-            arr4.push(evastandard.data[i]);
-        }
-    }
-
-    var jsRenderTpl = $.templates('#theTmpl1');
-    var finalTpl1 = jsRenderTpl(arr1);
-    console.log(arr1);
-    $('.box1').html(finalTpl1);
-
-
-    var jsRenderTpl = $.templates('#theTmpl2');
-    var finalTpl2 = jsRenderTpl(arr2);
-    console.log(arr2);
-    $('.box2').html(finalTpl2);
-
-    var jsRenderTpl = $.templates('#theTmpl3');
-    var finalTpl3 = jsRenderTpl(arr3);
-    console.log(arr3);
-    $('.box3').html(finalTpl3);
-
-    var finalTpl4 = jsRenderTpl(arr4);
-    console.log(arr4);
-    $('.box4').html(finalTpl4);
+            setInit(data);
         }
     });
 
-
    $(document).on('click','.savenowbtn',function(){
-    console.log(1111);
     var i = $(this).attr("id");
-    console.log(i);
     var changeCont = new Object();
-    changeCont.tid = i;
-    for(var k = 0; k < evastandard.length; k++) {
-        if(evastandard[k].id == i){
-            changeCont.table_name = evastandard[k].table_name;
-            changeCont.table_title = evastandard[k].item_title;
+    for(var k = 0; k < evastandard.data.length; k++) {
+        if(evastandard.data[k].id == i){
+            changeCont.tid = evastandard.data[k].tid;
+            changeCont.table_name = evastandard.data[k].table_name;
         }
     }
+    changeCont.item_title = $("#title"+i).val();
     changeCont.item_content = $("#content"+i).val();
     changeCont.item_point = $("#point"+i).val();
-    console.log(changeCont);
+    // var changeObj = new Object();
+    // changeObj.id = i;
+    // changeObj.content = changeCont;
+    // changeObj.operate = 1;
+    // changeArr.push(changeObj);
+    // console.log(changeArr);
+    $.ajax({
+        url: '../php/set_detail.php',
+        type: 'POST',
+        async: false,
+        data: {id: i, content: JSON.stringify(changeCont), operate: 1},
+        success: function(data) {
+            console.log(data);
+        }
+    });
+   });
+
+   $(document).on('click','.deletenowbtn',function(){
+    var i = $(this).attr("id");
     var changeObj = new Object();
     changeObj.id = i;
-    changeObj.content = changeCont;
-    changeObj.operate = 1;
-    console.log(changeObj);
+    changeObj.content = " ";
+    changeObj.operate = 3;
     changeArr.push(changeObj);
-   })
+    console.log(changeArr);
+    $("#only"+i).remove();
+    $.ajax({
+        url: '../php/set_detail.php',
+        type: 'POST',
+        async: false,
+        data: {id: i, content: " ", operate: 3},
+        success: function(data) {
+            console.log(data);
+        }
+    });
+   });
 
-//     $(".savenowbtn").click(function() {
-//     console.log(1111);
-//     var i = $(this).attr("id");
-//     console.log(i);
-//     var changeCont = new Object();
-//     changeCont.tid = i;
-//     for(var k = 0; k < evastandard.length; k++) {
-//         if(evastandard[k].id == i){
-//             changeCont.table_name = evastandard[k].table_name;
-//             changeCont.table_title = evastandard[k].item_title;
-//         }
-//     }
-//     changeCont.item_content = $("#content"+i).val();
-//     changeCont.item_point = $("#point"+i).val();
-//     console.log(changeCont);
-//     var changeObj = new Object();
-//     changeObj.id = i;
-//     changeObj.content = changeCont;
-//     changeObj.operate = 1;
-//     console.log(changeObj);
-//     changeArr.push(changeObj);
-// });
+   // $(document).on('click','#add1',function(){
+   //  $("#ul1").append("<li><div class=&Prime;standardbox&Prime;><div class=&Prime;item_title&Prime; style=&Prime;width: 15%&Prime;><textarea></textarea></div><div class=&Prime;item_content&Prime; style=&Prime;width: 60%&Prime;><textarea></textarea></div><div class=&Prime;item_point&Prime; style=&Prime;width: 15%&Prime;><textarea></textarea></div><div class=&Prime;savenow&Prime; style=&Prime;width: 10%&Prime;><button class=&Prime;savenowbtn&Prime;>保存修改</button></div><div class=&Prime;deletenow&Prime; style=&Prime;width: 10%&Prime;><button class=&Prime;deletenowbtn&Prime;>删除</button></div></div></li>");
+   // });
 
 });
 
 
 
-$("#standardSure").click(function() {
-    console.log(changeArr);
-    $.ajax({
-        url: '../php/set_detail.php',
-        type: 'POST',
-        async: false,
-        data: JSON.stringify(changeArr),
-        success: function(data) {
-            console.log(data);
-        }
-    });
-})
+// $("#standardSure").click(function() {
+//     console.log(changeArr);
+//     $.ajax({
+//         url: '../php/set_detail.php',
+//         type: 'POST',
+//         async: false,
+//         data: JSON.stringify(changeArr),
+//         success: function(data) {
+//             console.log(data);
+//         }
+//     });
+// })
 
 
 function cancel() {
